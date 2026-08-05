312332e2e278b474df9c8d03ff4e0c852a0fa270
Leslie Lamarche <leslie.lamarche@sri.com>
2026-08-05 08:42:16 -0700

    Clean old files from directory and fill bug fix.

--- instrumentation.py
+++ instrumentation.py
@@ -1,155 +1,18 @@
-# instrumentation.py
-# All the instrument/network classes
-
-import os
 import numpy as np
 import pymap3d as pm
-import pydarn
+import matplotlib.pyplot as plt
+import os
 import cartopy.crs as ccrs
-
-class ASINetwork(object):
-    #def __init__(self, network, label=None, elev=None, color=None, alt=None):
-    def __init__(self, name=None, elev=None, color=None, alt=None, sites=None):
-        #filename = os.path.join(os.path.dirname(__file__), 'site_data', '{}_sites.txt'.format(network))
-        #sites = np.loadtxt(filename, ndmin=2)
-        self.label = name
-        self.color = color
-
-        #self.elev = elev
-        #self.alt = alt
-
-        #self.sites = [ASI(site[0], site[1], site[2], elev, color, alt) for site in sites]
-        self.sites = [ASI(**site, elev=elev, alt=alt) for site in sites]
-
-#    def plot(self, ax):
-#
-#        for site in self.sites:
-#            glat, glon = site.generate_fov(self.elev, self.alt)
-#            ax.plot(glon, glat, transform=ccrs.Geodetic())
+import cartopy.feature as cfeature
+import pydarn
 
 
+def projected_beam(lat0, lon0, az, el, proj_alt=300.):
 
-class ASI(object):
-    def __init__(self, name=None, glat=None, glon=None, elev=None, alt=None):
-        self.site_lat = glat
-        self.site_lon = glon
-        self.site_alt = 0.
-#        self.color = color
-        #self.elev = elev
-
-        self.lat, self.lon = self.generate_fov(elev, alt)
-#
-    def generate_fov(self, elev, alt):
-        az = np.linspace(0., 360., 50)*np.pi/180.
-        el = np.full(50, elev)*np.pi/180.
-        lat, lon, alt = projected_beam(self.site_lat, self.site_lon, self.site_alt, az, el, proj_alt=alt)
-        return lat[::-1], lon[::-1]
-
-
-class FPINetwork(object):
-    #def __init__(self, network, label=None, elev=None, color=None, alt=None):
-    #    filename = os.path.join(os.path.dirname(__file__), 'site_data', '{}_sites.txt'.format(network))
-    #    sites = np.loadtxt(filename, ndmin=2)
-    #    self.label = label
-    #    self.color = color
-
-    #    self.sites = [FPI(site[0], site[1], site[2], elev, color, alt) for site in sites]
-    def __init__(self, name=None, elev=None, color=None, alt=None, sites=None):
-        self.label = name
-        self.color = color
-        self.sites = [FPI(**site, elev=elev, alt=alt) for site in sites]
-
-
-class FPI(object):
-    
-    def __init__(self, name=None, glat=None, glon=None, elev=None, alt=None):
-        self.site_lat = glat
-        self.site_lon = glon
-        self.site_alt = 0.
-        # self.elev = elev
-
-        self.lat, self.lon = self.generate_beams(elev, alt)
-
-    def generate_beams(self, elev, alt):
-        az = np.arange(0., 360., 90.)*np.pi/180.
-        el = np.full(4, elev)*np.pi/180.
-        lat, lon, alt = projected_beam(self.site_lat, self.site_lon, self.site_alt, az, el, proj_alt=alt)
-        return lat[::-1], lon[::-1]
-
-
-class AMISR(object):
-    # def __init__(self, site_lat, site_lon, site_alt, site_name, color):
-    def __init__(self, name=None, radar=None, alt=None, color=None):
-
-        site_coords = {'PFISR':[65.12992, -147.47104, 0.213], 'RISR-N':[74.72955, -94.90576, 0.145], 'RISR-C':[74.72955, -94.90576, 0.145]}
-        self.site_lat, self.site_lon, self.site_alt = site_coords[radar]
-        self.color = color
-
-        az, el = self.load_data(radar)
-        self.lat, self.lon = self.generate_fov(az, el)
-
-    def load_data(self, radar):
-        filename = os.path.join(os.path.dirname(__file__), 'site_data', '{}GratingLimits.txt'.format(radar.replace('-','').lower()))
-        data = np.loadtxt(filename)
-        az = data[:,0]*np.pi/180.
-        el = data[:,1]*np.pi/180.
-        return az, el
-
-    def generate_fov(self, az, el):
-        lat, lon, alt = projected_beam(self.site_lat, self.site_lon, 0., az, el, proj_alt=450)
-        return lat[::-1], lon[::-1]
-
-class ISR(object):
-    def __init__(self, name=None, glat=None, glon=None, elev=None, alt=None, color=None):
-        self.site_lat = glat
-        self.site_lon = glon
-        self.color = color
-
-        self.lat, self.lon = self.generate_fov(elev)
-
-    def generate_fov(self, elev):
-        az = np.linspace(0., 360., 50)*np.pi/180.
-        el = np.full(50, elev)*np.pi/180.
-        lat, lon, alt = projected_beam(self.site_lat, self.site_lon, 0., az, el, proj_alt=450)
-        return lat[::-1], lon[::-1]
-
-class SuperMAG(object):
-    def __init__(self, name=None, color=None):
-        self.color = color
-        filename = os.path.join(os.path.dirname(__file__), 'site_data', 'SuperMAG_sites.txt')
-        data = np.loadtxt(filename,skiprows=44,usecols=(1,2))
-        self.sites = np.array([data[:,1],data[:,0],np.zeros(data.shape[0])]).T
-
-class SDNetwork(object):
-    def __init__(self, name=None, color=None, radars=None):
-        self.color = color
-
-        radar_info = pydarn.SuperDARNRadars.radars
-
-        if radars:
-            self.sites = [SuperDARN(stid) for stid, info in radar_info.items() if info.hardware_info.abbrev in radars]
-        else:
-            self.sites = [SuperDARN(stid) for stid in radar_info.keys()]
-
-
-class SuperDARN(object):
-    def __init__(self, stid):
-
-        gate_lat, gate_lon = pydarn.Coords.GEOGRAPHIC(stid)
-        self.lat = np.concatenate((gate_lat[0,:],gate_lat[:,-1],gate_lat[-1,::-1],gate_lat[::-1,0]))
-        self.lon = np.concatenate((gate_lon[0,:],gate_lon[:,-1],gate_lon[-1,::-1],gate_lon[::-1,0]))
-
-
-
-def projected_beam(lat0,lon0,alt0,az,el,proj_alt=300.):
-
-#     lat0, lon0, alt0 = site
-#     az = az*np.pi/180.
-#     el = el*np.pi/180.
-
-    x, y, z = pm.geodetic2ecef(lat0, lon0, alt0*1000.)
+    x, y, z = pm.geodetic2ecef(lat0, lon0, 0.)
     vx, vy, vz = pm.enu2uvw(np.cos(el)*np.sin(az), np.cos(el)*np.cos(az), np.sin(el), lat0, lon0)
 
+    #earth = pm.Ellipsoid()
     earth = pm.Ellipsoid.from_name('wgs84')
     a2 = (earth.semimajor_axis + proj_alt*1000.)**2
     b2 = (earth.semimajor_axis + proj_alt*1000.)**2
@@ -164,3 +27,156 @@ def projected_beam(lat0,lon0,alt0,az,el,proj_alt=300.):
     lat, lon, alt = pm.ecef2geodetic(x + alpha*vx, y + alpha*vy, z + alpha*vz)
 
     return lat, lon, alt/1000.
+
+
+def generate_asi_fov(site_lat, site_lon, elev, alt, npoint=50):
+    az = np.linspace(0., 360., npoint)*np.pi/180.
+    el = np.full(npoint, elev)*np.pi/180.
+    lat, lon, alt = projected_beam(site_lat, site_lon, az, el, proj_alt=alt)
+    return lat[::-1], lon[::-1]
+
+def generate_fpi_beams(site_lat, site_lon, elev, alt):
+    az = np.arange(0., 360., 90.)*np.pi/180.
+    el = np.full(4, elev)*np.pi/180.
+    lat, lon, alt = projected_beam(site_lat, site_lon, az, el, proj_alt=alt)
+    return lat[::-1], lon[::-1]
+
+
+def generate_amisr_fov(site_lat, site_lon, alt, radar):
+    filename = os.path.join(os.path.dirname(__file__), 'site_data', '{}GratingLimits.txt'.format(radar.replace('-','').lower()))
+    az, el = np.loadtxt(filename, usecols=[0,1], unpack=True)
+    az = np.deg2rad(az)
+    el = np.deg2rad(el)
+
+    lat, lon, alt = projected_beam(site_lat, site_lon, az, el, proj_alt=alt)
+    return lat[::-1], lon[::-1]
+
+
+def get_mag_sites():
+    filename = os.path.join(os.path.dirname(__file__), 'site_data', 'SuperMAG_sites.txt')
+    lon, lat = np.loadtxt(filename, skiprows=44, usecols=(1,2), unpack=True)
+    return lat, lon
+
+
+def generate_sd_fov(radar):
+
+    hdw_data = pydarn.read_hdw_file(radar)
+    gate_lat, gate_lon = pydarn.Coords.GEOGRAPHIC(pydarn.RadarID(hdw_data.stid))
+
+    lat = np.concatenate((gate_lat[0,:],gate_lat[:,-1],gate_lat[-1,::-1],gate_lat[::-1,0]))
+    lon = np.concatenate((gate_lon[0,:],gate_lon[:,-1],gate_lon[-1,::-1],gate_lon[::-1,0]))
+    return lat, lon
+
+
+
+def ASI(ax, glat=None, glon=None, elev=None, alt=None, **plotting_params):
+
+    fov_lat, fov_lon = generate_asi_fov(glat, glon, elev, alt)
+
+    if 'fill' in plotting_params:
+        fill_params = plotting_params['fill'].copy()
+        if 'color' not in fill_params:
+            fill_params['color'] = plotting_params['color']
+        ax.fill(fov_lon, fov_lat, transform=ccrs.Geodetic(), **fill_params)
+
+    nonplotkw = ['name','sites','fill']
+    for kw in nonplotkw:
+        if kw in plotting_params:
+            del plotting_params[kw]
+
+    ax.plot(fov_lon, fov_lat, transform=ccrs.Geodetic(), **plotting_params)
+
+
+
+def FPI(ax, glat=None, glon=None, elev=None, alt=None, **plotting_params):
+
+    fov_lat, fov_lon = generate_fpi_beams(glat, glon, elev, alt)
+
+    nonplotkw = ['name','sites']
+    for kw in nonplotkw:
+        del plotting_params[kw]
+
+    ax.scatter(fov_lon, fov_lat, transform=ccrs.Geodetic(), **plotting_params)
+
+
+def SDI(ax, glat=None, glon=None, elev=None, alt=None, **plotting_params):
+
+    fov_lat, fov_lon = generate_asi_fov(glat, glon, elev, alt)
+
+    if 'fill' in plotting_params:
+        fill_params = plotting_params['fill'].copy()
+        if 'color' not in fill_params:
+            fill_params['color'] = plotting_params['color']
+        ax.fill(fov_lon, fov_lat, transform=ccrs.Geodetic(), **fill_params)
+
+    nonplotkw = ['name','sites','fill']
+    for kw in nonplotkw:
+        if kw in plotting_params:
+            del plotting_params[kw]
+
+    ax.plot(fov_lon, fov_lat, transform=ccrs.Geodetic(), **plotting_params)
+
+
+
+# Plot ISRs
+def ISR(ax, glat=None, glon=None, elev=None, alt=None, **plotting_params):
+    fov_lat, fov_lon = generate_asi_fov(glat, glon, elev, alt)
+
+    if 'fill' in plotting_params:
+        fill_params = plotting_params['fill'].copy()
+        if 'color' not in fill_params:
+            fill_params['color'] = plotting_params['color']
+        ax.fill(fov_lon, fov_lat, transform=ccrs.Geodetic(), **fill_params)
+
+    nonplotkw = ['name', 'fill']
+    for kw in nonplotkw:
+        if kw in plotting_params:
+            del plotting_params[kw]
+
+    ax.plot(fov_lon, fov_lat, transform=ccrs.Geodetic(), **plotting_params)
+
+
+# Plot AMISRs
+def AMISR(ax, glat=None, glon=None, alt=None, radar=None, **plotting_params):
+#for site in instruments['AMISR']:
+    fov_lat, fov_lon = generate_amisr_fov(glat, glon, alt, radar)
+
+    if 'fill' in plotting_params:
+        fill_params = plotting_params['fill'].copy()
+        if 'color' not in fill_params:
+            fill_params['color'] = plotting_params['color']
+        ax.fill(fov_lon, fov_lat, transform=ccrs.Geodetic(), **fill_params)
+
+    nonplotkw = ['name','fill']
+    for kw in nonplotkw:
+        if kw in plotting_params:
+            del plotting_params[kw]
+
+    ax.plot(fov_lon, fov_lat, transform=ccrs.Geodetic(), **plotting_params)
+
+# Plot SuperMAG
+def SuperMAG(ax, **plotting_params):
+    fov_lat, fov_lon = get_mag_sites()
+
+    nonplotkw = ['name']
+    for kw in nonplotkw:
+        del plotting_params[kw]
+
+    ax.scatter(fov_lon, fov_lat, transform=ccrs.Geodetic(), **plotting_params)
+
+# Plot SuperDARN
+def SuperDARN(ax, name=None, **plotting_params):
+    fov_lat, fov_lon = generate_sd_fov(name)
+
+    if 'fill' in plotting_params:
+        fill_params = plotting_params['fill'].copy()
+        if 'color' not in fill_params:
+            fill_params['color'] = plotting_params['color']
+        ax.fill(fov_lon, fov_lat, transform=ccrs.Geodetic(), **fill_params)
+
+    nonplotkw = ['sites','fill']
+    for kw in nonplotkw:
+        if kw in plotting_params:
+            del plotting_params[kw]
+
+    ax.plot(fov_lon, fov_lat, transform=ccrs.Geodetic(), **plotting_params)
--- map_facilities.py
+++ map_facilities.py
@@ -8,7 +8,7 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import yaml
 import pydarn
-import instrumentation2 as instrumentation
+import instrumentation as instrumentation
 
 # Specify where to find cartopy backgrounds locally
 # THIS WILL HAVE TO BE CHANGED FOR EACH USER
@@ -20,10 +20,10 @@ import os
 os.environ['CARTOPY_USER_BACKGROUNDS'] = '/Users/e30737/Desktop/Data/cartopy_background'
 
 # Specify output figure file name
-output_figure = 'AK_map.png'
+output_figure = 'ASI_map.png'
 
 # Read in sites file which specifies instruments
-with open('ak_sites.yaml', 'r') as f:
+with open('sites.yaml', 'r') as f:
     instruments = yaml.safe_load(f)
 
 print(instruments.keys())
@@ -53,12 +53,12 @@ print(instruments.keys())
 #
 #cent_glat = (min(glat_list) + max(glat_list))/2.
 #cent_glon = (min(glon_list) + max(glon_list))/2.
-#cent_glat = 50.
-#cent_glon = 250.
+cent_glat = 50.
+cent_glon = 250.
 
-# Centeral Alaska
-cent_glat = 65.5
-cent_glon = -147.7
+## Centeral Alaska
+#cent_glat = 65.5
+#cent_glon = -147.7
 
 
 # Set up figure
@@ -67,8 +67,8 @@ proj = ccrs.Orthographic(central_longitude=cent_glon, central_latitude=cent_glat
 ax = plt.subplot(111, projection=proj)
 ax.coastlines(resolution='50m',zorder=0.5)
 ax.gridlines()
-#ax.set_extent([-155, -60, 20, 80], crs=ccrs.PlateCarree())
-ax.set_extent([-170, -125, 50, 75], crs=ccrs.PlateCarree())
+ax.set_extent([-155, -60, 20, 80], crs=ccrs.PlateCarree())
+#ax.set_extent([-170, -125, 50, 75], crs=ccrs.PlateCarree())
 
 # Add all instruments from sites.yaml
 for inst_type, values in instruments.items():
@@ -97,7 +97,6 @@ for inst_type, values in instruments.items():
     # Plot individual instruments
     if 'sites' in values:
         for site in values['sites']:
-            print(site['name'])
             params = default_params | site
             params['label'] = params['name']
             plot_funct(ax, **params)
